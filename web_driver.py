d64f65ad82f86e78be2f31fbbcf9376316fd0ea3
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-13 20:08:05 +0300

    first commit

---
+++ web_driver.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+options = webdriver.ChromeOptions()
+
+options.add_experimental_option("detach", True)
+
+options.add_argument("--guest")
+# options.add_argument("--incognito")
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+base_url = 'https://www.saucedemo.com/'
+
+driver.get(base_url)
+
+driver.maximize_window()
