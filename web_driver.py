1c39fa8fca5cb17513f977d5adfdda26def6d128
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-14 19:43:14 +0300

    small changes

--- web_driver.py
+++ web_driver.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 
 options = webdriver.ChromeOptions()
@@ -12,5 +13,14 @@ driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverMan
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
-time.sleep(10)
-driver.close()
+# userName = driver.find_elements_by_id("user-name")
+# userName = driver.find_element(By.ID, "user-name") # ID
+# userName = driver.find_element(By.NAME, "user-name") # NAME
+# userName = driver.find_element(By.XPATH, "//*[@id='user-name']") # xpath full
+# userName = driver.find_element(By.XPATH, "//input[@id='user-name']") # xpath
+userName = driver.find_element(By.XPATH, "//input[@data-test='username']") # xpath data-test
+userName.send_keys("standard_user")
+
+
+# time.sleep(10)
+# driver.close()
