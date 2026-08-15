e9ae38c88f85b8be711e86bf0f6b27004af541e6
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-15 10:11:03 +0300

    first test

---
+++ test_1.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("--guest")
+# options.add_argument("--incognito")
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+LoginStandartUser = "standard_user"
+passwordAll = "secret_sauce"
+
+
+userName = driver.find_element(By.XPATH, "//input[@id='user-name']")
+userName.send_keys(LoginStandartUser)
+print("Input Login")
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(passwordAll)
+print("Input Password")
+buttonLogin = driver.find_element(By.XPATH, "//input[@id='login-button']")
+buttonLogin.click()
+print("Click login Button")
+textProducts = driver.find_element(By.XPATH, "//span[@class='title']")
+vlueTextProducts = textProducts.text # считываем значение текста в локатаре, запомнили его и сохранили в переменную
+print(vlueTextProducts)
+assert vlueTextProducts == "Products"
+print("Good")
+
+url = "https://www.saucedemo.com/inventory.html"
+getUrl = driver.current_url
+print(getUrl)
+assert url == getUrl
+print("Good url")
