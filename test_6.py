9bd2c60a6bd94249155b9306f4481c0dc1aaaedb
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-17 19:11:13 +0300

    add new test

---
+++ test_6.py
@@ -0,0 +1,27 @@
+import datetime
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+# options.add_argument("--headless")
+options.add_argument("--guest")
+g = Service()
+driver = webdriver.Chrome(options=options, service=g)
+driver.set_window_size(1920, 1080)
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+
+LoginStandardUser = "standard_user"
+passwordAll = "secret_sauce"
+
+userName = driver.find_element(By.XPATH, "//input[@id='user-name']")
+userName.send_keys(LoginStandardUser)
+print("Input Login")
+
+time.sleep(5)
+userName.clear()
+
