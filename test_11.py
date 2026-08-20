2e0236e25912b3961b4ddf6caf7aafab0e271dd9
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-20 18:36:33 +0300

    action right click and double click

---
+++ test_11.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("--guest")
+
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920, 1080)
+
+base_url = "https://demoqa.com/"
+driver.get(base_url + "buttons")
+
+action = ActionChains(driver)
+
+doubleClick = driver.find_element(By.XPATH, "//button[@id='doubleClickBtn']")
+action.double_click(doubleClick).perform()
+
+rightClick = driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
+action.context_click(rightClick).perform()
