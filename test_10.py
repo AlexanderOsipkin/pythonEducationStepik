b786c72a1bd30888f5c5cdca38637bb7790a6b33
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-08-20 18:28:45 +0300

    radio button

---
+++ test_10.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("--guest")
+
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920, 1080)
+
+base_url = "https://demoqa.com/"
+driver.get(base_url + "radio-button")
+
+radioButton = driver.find_element(By.XPATH, "//input[@id='yesRadio']")
+radioButton.click()
